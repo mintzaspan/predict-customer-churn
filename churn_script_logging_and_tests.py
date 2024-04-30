27801230f81435ad64a488a65e55d65e10c6c71e
mincorp <mincorp@users.noreply.github.com>
2024-04-30 15:19:15 +0100

    fixes and load model test

--- churn_script_logging_and_tests.py
+++ churn_script_logging_and_tests.py
@@ -10,7 +10,7 @@ logging.basicConfig(
     format='%(name)s - %(levelname)s - %(message)s')
 
 
-def test_import(import_data):
+def test_import():
     """Test the import_data function
     """
     try:
@@ -32,11 +32,11 @@ def test_import(import_data):
 @pytest.fixture
 def df():
     data = pd.DataFrame({
-        'num_col1': [1, 2, 3, 4, 5],
-        'num_col2': [5, 4, 3, 2, 1],
-        'cat_col1': ['a', 'b', 'a', 'b', 'a'],
-        'cat_col2': ['b', 'a', 'b', 'a', 'b'],
-        'target_col': [1, 0, 1, 0, 1]
+        'num_col1': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+        'num_col2': [5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1],
+        'cat_col1': ['a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a'],
+        'cat_col2': ['b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b'],
+        'target_col': [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
     })
     return data
 
@@ -48,14 +48,16 @@ def test_perform_eda(df):
     perform_eda(df, num_cols, cat_cols, target_col)
 
     # Check if images are created
-    for col in num_cols + cat_cols:
+    for col in num_cols:
         assert os.path.exists(f'images/eda/{col}_histplot.png')
+    for col in cat_cols:
         assert os.path.exists(f'images/eda/{col}_freqplot.png')
         assert os.path.exists(f'images/eda/{col}_bv_plot.png')
 
 
 def test_split_frame(df):
-    X_train, X_test, y_train, y_test = split_frame(df, 'target_col')
+    X_train, X_test, y_train, y_test = split_frame(
+        df=df, response='target_col', test_size=0.2)
     assert X_train.shape[0] == y_train.shape[0]
     assert X_test.shape[0] == y_test.shape[0]
     assert X_train.shape[1] == X_test.shape[1]
@@ -65,12 +67,13 @@ def test_split_frame(df):
 def test_train_model(df):
     num_cols = ['num_col1', 'num_col2']
     cat_cols = ['cat_col1', 'cat_col2']
-    target_col = 'target_col'
-    model = train_model(df, num_cols, cat_cols, target_col)
+    algo = 'logistic_regression'
+    X_train, X_test, y_train, y_test = split_frame(df, 'target_col', 0.2)
+    model = train_model(algo, X_train, y_train, num_cols, cat_cols)
     assert model is not None
-    assert os.path.exists('models/model.pkl')
+    assert os.path.exists('models/logistic_regression.pkl')
 
 
 def test_load_model():
-    model = load_model('models/model.pkl')
+    model = load_model('models/logistic_regression.pkl')
     assert model is not None
