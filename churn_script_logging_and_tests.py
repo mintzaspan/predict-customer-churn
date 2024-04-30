39cc18bdf3c5080b2f1cb681575f16767a805f65
mincorp <mincorp@users.noreply.github.com>
2024-04-30 14:15:52 +0100

    import data test

---
+++ churn_script_logging_and_tests.py
@@ -0,0 +1,28 @@
+import os
+import logging
+import churn_library as cls
+
+logging.basicConfig(
+    filename='./logs/churn_script_logging_and_tests.log',
+    level=logging.INFO,
+    filemode='w',
+    format='%(name)s - %(levelname)s - %(message)s')
+
+
+def test_import(import_data):
+    """Test the import_data function
+    """
+    try:
+        df = import_data("./data/bank_data.csv")
+        logging.info("Testing import_data: SUCCESS")
+    except FileNotFoundError as err:
+        logging.error("Testing import_eda: The file wasn't found")
+        raise err
+
+    try:
+        assert df.shape[0] > 0
+        assert df.shape[1] > 0
+    except AssertionError as err:
+        logging.error(
+            "Testing import_data: The file doesn't appear to have rows and columns")
+        raise err
